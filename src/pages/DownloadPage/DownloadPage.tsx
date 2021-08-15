c58d44ccaf36cae0cb4a959ccfba73d69dcb4d12
dennisb <dennis.borshchevski@cloudinary.com>
2021-08-15 15:02:29 +0300

    cleanup

--- src/pages/DownloadPage/DownloadPage.tsx
+++ src/pages/DownloadPage/DownloadPage.tsx
@@ -4,7 +4,7 @@ import { jobState } from "../../state/selectors";
 import { useRootStyles } from "../Editor/EditorPage";
 import { Box, Grid } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
-import { useFetch } from "use-http";
+import { CachePolicies, useFetch } from "use-http";
 import { AUTH } from "../../constants";
 import { parseData } from "../../utils/xml.utils";
 import { StyledImage, StyledImageContainer } from './DownloadPage.styled';
@@ -29,7 +29,10 @@ function createAndDownloadBlobFile(body: any, filename: string, extension = 'jpe
 export const DownloadPage = () => {
   const job = useRecoilValue(jobState);
   const classes = useRootStyles();
-  const { post } = useFetch('https://ids.w2p-tools.com')
+  const { post } = useFetch('https://ids.w2p-tools.com/d003/requestpreview.php', {
+    cache: 'no-cache',
+    cachePolicy: CachePolicies.NO_CACHE
+  })
   const { post: postGetImage } = useFetch('https://ids.w2p-tools.com', { responseType: 'blob' })
 
   const download = async (type: string) => {
@@ -38,9 +41,10 @@ export const DownloadPage = () => {
     formData.append("id", job.value);
     formData.append("type", type);
 
-    const previewRequestXml = await post('/d003/requestpreview.php', formData)
+    const previewRequestXml = await post(formData)
     const previewData = parseData(previewRequestXml)
-    const { loc } = previewData['jpeg-preview'].attr;
+    console.log('previewData', previewData)
+    const { loc } = previewData[`${type}-preview`].attr;
     formData.append("loc", loc)
     setTimeout(async () => {
       const data = await postGetImage('/d003/getpreview.php', formData)
